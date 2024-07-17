51652aedfea47957c15bcb706eba67b674731a6a
Keith Bartholomew <keith.bartholomew@gmail.com>
2024-07-16 21:20:36 -0500

    food, games, entertainment

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
@@ -35,77 +36,91 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <nav className="z-10 text-lg bg-teal-800 font-bold w-full text-white">
-        <ul className="flex justify-center">
+      <nav className={styles.headerNav}>
+        <ul className="md:flex justify-center">
           <li>
-            <a
-              className="block p-6 hover:bg-teal-600 transition-colors"
-              href="#visit"
-            >
+            <a className={styles.headerNav__link} href="#visit">
               Plan Your Visit
             </a>
           </li>
           <li>
-            <a
-              className="block p-6 hover:bg-teal-600 transition-colors"
-              href="#food-and-games"
-            >
+            <a className={styles.headerNav__link} href="#food-and-games">
               Food &amp; Games
             </a>
           </li>
           <li>
-            <a
-              className="block p-6 hover:bg-teal-600 transition-colors"
-              href="#vendors"
-            >
+            <a className={styles.headerNav__link} href="#entertainment">
+              Entertainment
+            </a>
+          </li>
+          <li>
+            <a className={styles.headerNav__link} href="#vendors">
               Vendors
             </a>
           </li>
           <li>
-            <a
-              className="block p-6 hover:bg-teal-600 transition-colors"
-              href="#about-us"
-            >
+            <a className={styles.headerNav__link} href="#about-us">
               About Us
             </a>
           </li>
         </ul>
       </nav>
-      <section className="py-16 max-w-[80vw] mx-auto">
-        <h2 id="visit">Plan Your Visit</h2>
-        <div className="grid grid-cols-2 gap-4">
+      <section id="visit" className={styles.pageSection}>
+        <div className={styles.pageSection__grid}>
           <div>
+            <h2>Plan Your Visit</h2>
             <p className="text-2xl my-4">
               Join us for a weekend of fun and festivity at{" "}
               <strong>GorettiFest</strong>, a fall festival benefiting Saint
               Maria Goretti Catholic School!
             </p>
-            <table>
+            <table className={styles.infoTable}>
               <tbody>
                 <tr>
-                  <th className="py-2 pr-8 align-top">When</th>
-                  <td className="py-2">
+                  <th>When</th>
+                  <td>
                     Saturday, October 5 from 11am to 8pm <br />
                     Sunday, October October 6 from 8am to 3pm
                   </td>
                 </tr>
                 <tr>
-                  <th className="py-2 pr-8 align-top">Where</th>
-                  <td className="py-2">
+                  <th>Where</th>
+                  <td>
                     <strong>Saint Maria Goretti Catholic School</strong>
                     <br />
-                    <address>
-                      1200 S Davis Dr <br />
-                      Arlington, TX 76013
-                    </address>
+                    <a
+                      href="https://maps.app.goo.gl/qKE7DvAxQKcZ7Qyt9"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <address>
+                        1200 S Davis Dr <br />
+                        Arlington, TX 76013
+                      </address>
+                    </a>
                   </td>
                 </tr>
                 <tr>
-                  <th className="py-2 pr-8 align-top">Cost</th>
-                  <td className="py-2">
+                  <th>Cost</th>
+                  <td>
                     Admission is <strong>FREE</strong>! <br />
                     Buy tickets for food &amp; games with cash or card after you
                     arrive.
+                  </td>
+                </tr>
+                <tr>
+                  <th>Parking</th>
+                  <td>
+                    Free parking is available in our{" "}
+                    <a
+                      href="https://maps.app.goo.gl/cqx3drVKb1xtRaeN6"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      parking lot on Benge Drive
+                    </a>
+                    . It's just a &frac14; mile walk from the parking lot to the
+                    school.
                   </td>
                 </tr>
               </tbody>
@@ -124,8 +139,130 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="py-16 max-w-[80vw] mx-auto">
-        <h2 id="food-and-games">Food &amp; Games</h2>
+      <section id="food-and-games" className="bg-amber-100">
+        <div className={styles.pageSection}>
+          <div className={styles.pageSection__grid}>
+            <div>
+              <h2>Food &amp; Games</h2>
+              <p className="text-2xl">
+                For generations, homemade cuisine from around the world has
+                defined GorettiFest. Enjoy a break between snacks and games in
+                our biergarten!
+              </p>
+              <p className="text-2xl"></p>
+              <table className={styles.infoTable}>
+                <tbody>
+                  <tr>
+                    <th>American Food</th>
+                    <td>Hamburgers, Nachos, French fries</td>
+                  </tr>
+                  <tr>
+                    <th>German Food</th>
+                    <td>
+                      Bratwurst, Sausage on a stick, Sauerkraut, Fried potatoes
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Mexican Food</th>
+                    <td>Fajitas, Nachos, Breakfast burritos (on Sunday)</td>
+                  </tr>
+                  <tr>
+                    <th>Caribbean Food</th>
+                    <td>
+                      Beignets, Jerk chicken, Jollof rice (spiced fried rice)
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Asian Food</th>
+                    <td>Chow mein, Egg rolls, Fried rice</td>
+                  </tr>
+                  <tr>
+                    <th>Sweets &amp; Drinks</th>
+                    <td>
+                      Paletas (popsicles), Craft on draft biergarten, Soft
+                      drinks, Water
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div>
+              <Image
+                className="border-[1rem] border-white drop-shadow-lg rotate-2"
+                src="/img/IMG_1246.webp"
+                alt="a young boy enjoying frozen treats at GorettiFest festival"
+                width={900}
+                height={900}
+              />
+            </div>
+
+            <div>
+              <Image
+                className="border-[1rem] border-white drop-shadow-lg -rotate-2"
+                src="/img/IMG_1358.webp"
+                alt="a young boy enjoying frozen treats at GorettiFest festival"
+                width={900}
+                height={900}
+              />
+            </div>
+            <div>
+              <p className="text-2xl">
+                Test your skills at our annual Cornhole tournament, press your
+                luck at our carnival games or bingo hall, or set the little ones
+                loose in our (supervised) bounce houses!
+              </p>
+              <table className={styles.infoTable}>
+                <tbody>
+                  <tr>
+                    <th>Cornhole tournament</th>
+                    <td>
+                      <a href="">Register in advance</a> to enter. Cash prize
+                      for the winning team!
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Bingo</th>
+                    <td>
+                      Real bingo with a professional caller and cash prizes
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Jail</th>
+                    <td>
+                      Buy a warrant to put your friends in jail, or pay a bounty
+                      to incarcerate the Principal or Priest! (if you can catch
+                      them)
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Carnival Games</th>
+                    <td>Nerf shooting gallery, Marble races</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="entertainment" className={styles.pageSection}>
+        <div className={styles.pageSection__grid}>
+          <div>
+            <Image
+              className="border-[1rem] border-white drop-shadow-lg rotate-2"
+              src="/img/IMG_1433.webp"
+              alt="a young boy enjoying frozen treats at GorettiFest festival"
+              width={900}
+              height={900}
+            />
+          </div>
+          <div>
+            <h2>Entertainment</h2>
+            <p className="text-2xl">
+              Our main stage has exhibitions from our school band, "Ballet
+              Folklorico" dance troupe, martial artists, and other live music!
+            </p>
+          </div>
+        </div>
       </section>
     </main>
   );
