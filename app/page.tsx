02c62c17e8ee38b906d2bf0364f490744e57cec1
Keith Bartholomew <keith.bartholomew@gmail.com>
2024-07-16 19:45:40 -0500

    initial-ish commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,112 +2,131 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="">
+      <section className="relative h-[80vh]">
+        <div className="absolute h-full overflow-hidden">
+          <Image
+            className="h-full object-cover"
+            src="/img/IMG_1440.webp"
+            alt="picture"
+            width={1800}
+            height={1800}
+          />
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div
+          className="absolute h-full w-full"
+          style={{ background: "rgba(0,0,0,0.6)" }}
+        ></div>
+        <div className="flex h-full w-full absolute text-center items-center justify-center">
+          <div>
+            <Image
+              src="/img/goretti-fest-logo.svg"
+              alt="Goretti Fest Logo"
+              width={500}
+              height={500}
+            />
+            <div className="mt-8 inline-block flex-col bg-white text-2xl rounded-lg">
+              <div className="bg-red-700 border-b-2 border-b-red-900 border-t border-t-red-400 text-white text-center py-4 px-8 rounded-t-lg">
+                October
+              </div>
+              <div className="text-6xl py-4 px-8 tracking-tighter">5 - 6</div>
+              <div className="py-2 px-8">2024</div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <nav className="z-10 text-lg bg-teal-800 font-bold w-full text-white">
+        <ul className="flex justify-center">
+          <li>
+            <a
+              className="block p-6 hover:bg-teal-600 transition-colors"
+              href="#visit"
+            >
+              Plan Your Visit
+            </a>
+          </li>
+          <li>
+            <a
+              className="block p-6 hover:bg-teal-600 transition-colors"
+              href="#food-and-games"
+            >
+              Food &amp; Games
+            </a>
+          </li>
+          <li>
+            <a
+              className="block p-6 hover:bg-teal-600 transition-colors"
+              href="#vendors"
+            >
+              Vendors
+            </a>
+          </li>
+          <li>
+            <a
+              className="block p-6 hover:bg-teal-600 transition-colors"
+              href="#about-us"
+            >
+              About Us
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <section className="py-16 max-w-[80vw] mx-auto">
+        <h2 id="visit">Plan Your Visit</h2>
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <p className="text-2xl my-4">
+              Join us for a weekend of fun and festivity at{" "}
+              <strong>GorettiFest</strong>, a fall festival benefiting Saint
+              Maria Goretti Catholic School!
+            </p>
+            <table>
+              <tbody>
+                <tr>
+                  <th className="py-2 pr-8 align-top">When</th>
+                  <td className="py-2">
+                    Saturday, October 5 from 11am to 8pm <br />
+                    Sunday, October October 6 from 8am to 3pm
+                  </td>
+                </tr>
+                <tr>
+                  <th className="py-2 pr-8 align-top">Where</th>
+                  <td className="py-2">
+                    <strong>Saint Maria Goretti Catholic School</strong>
+                    <br />
+                    <address>
+                      1200 S Davis Dr <br />
+                      Arlington, TX 76013
+                    </address>
+                  </td>
+                </tr>
+                <tr>
+                  <th className="py-2 pr-8 align-top">Cost</th>
+                  <td className="py-2">
+                    Admission is <strong>FREE</strong>! <br />
+                    Buy tickets for food &amp; games with cash or card after you
+                    arrive.
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="bg-teal-800 p-8 rounded-2xl">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3356.5415387098847!2d-97.12749252363562!3d32.72480818665103!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e7d1a02bc13ef%3A0xc77ecf1dab906f16!2sSt%20Maria%20Goretti%20Catholic%20School!5e0!3m2!1sen!2sus!4v1721175085047!5m2!1sen!2sus"
+              width="600"
+              height="450"
+              className="w-full"
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </div>
+      </section>
+      <section className="py-16 max-w-[80vw] mx-auto">
+        <h2 id="food-and-games">Food &amp; Games</h2>
+      </section>
     </main>
   );
 }
