e096790875b7ffc2f6d38d9b36a87e32612196de
Keith Bartholomew <keith.bartholomew@gmail.com>
2024-08-04 10:45:10 -0500

    add toggle for menu on small screens

--- app/cornhole/register/callback/page.tsx
+++ app/cornhole/register/callback/page.tsx
@@ -30,7 +30,7 @@ export default function CornholeRegisterCallback() {
           />
         </div>
         <div className="absolute top-0 left-0 h-full w-full mix-blend-multiply bg-slate-800 md:bg-slate-900/70"></div>
-        <div className="py-16">
+        <div className="md:py-16">
           <div
             id="cornhole-signup"
             className="bg-white relative md:max-w-[67vw] lg:max-w-[50vw] mx-auto md:rounded-3xl p-8 md:drop-shadow-xl"
--- components/header-nav.tsx
+++ components/header-nav.tsx
@@ -1,7 +1,52 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./header-nav.module.css";
+import { MouseEventHandler, useState } from "react";
+
+function MobileNavToggle({
+  show,
+  onClick,
+}: {
+  show: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}) {
+  return (
+    <button
+      aria-label="Toggle Navigation"
+      className="md:hidden text-center block w-full py-2 bg-slate-800 hover:bg-slate-700"
+      onClick={onClick ?? function () {}}
+    >
+      <span
+        className={`inline-block transition-transform ${
+          show ? "scale-y-[-1]" : ""
+        }`}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="inline-block size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m19.5 8.25-7.5 7.5-7.5-7.5"
+          />
+        </svg>
+      </span>
+    </button>
+  );
+}
 
 export function HeaderNav({ logo }: { logo?: boolean }) {
+  const [showMobileNav, setShowMobileNav] = useState(false);
+  const toggleNav = () => {
+    setShowMobileNav(!showMobileNav);
+  };
+
   return (
     <nav className={styles.headerNav}>
       {logo ? (
@@ -19,7 +64,12 @@ export function HeaderNav({ logo }: { logo?: boolean }) {
           </div>
         </a>
       ) : null}
-      <ul className="md:flex justify-center">
+      <MobileNavToggle onClick={toggleNav} show={showMobileNav} />
+      <ul
+        className={`${
+          showMobileNav ? "block" : "hidden"
+        } md:flex justify-center`}
+      >
         <li>
           <a className={styles.headerNav__link} href="/#visit">
             Plan Your Visit
