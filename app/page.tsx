984c50b436fa34d927cde6bd1ba82a7649b03804
Keith Bartholomew <keith.bartholomew@gmail.com>
2024-09-23 11:39:50 -0500

    many sponsors

--- app/page.tsx
+++ app/page.tsx
@@ -42,6 +42,84 @@ const eventStructuredData = {
   },
 };
 
+type SponsorLogo = {
+  size: "small" | "large";
+  imageUrl: string;
+  linkUrl?: string;
+  name: string;
+};
+
+const sponsors: SponsorLogo[] = [
+  {
+    size: "large",
+    name: "Barnes Video Group",
+    imageUrl: "/img/sponsors/barnes-video-group-logo.png",
+    linkUrl: "https://www.barnesvideogroup.com/",
+  },
+  {
+    size: "large",
+    name: "Rick and Ray's Auto Plaza",
+    imageUrl: "/img/sponsors/rick-and-rays-logo.png",
+    linkUrl: "https://rickandraysautoplaza.com/",
+  },
+  {
+    size: "large",
+    name: "The Keever Group Amerprise Wealth Advisors",
+    imageUrl: "/img/sponsors/the-keever-group-logo.png",
+    linkUrl: "https://www.ameripriseadvisors.com/team/the-keever-group/",
+  },
+  {
+    size: "small",
+    name: "Knights of Columbus",
+    imageUrl: "/img/sponsors/KOC.png",
+  },
+  {
+    size: "small",
+    name: "Affalon Productions",
+    imageUrl: "/img/sponsors/Affalon%20Productions.png",
+  },
+  {
+    size: "small",
+    name: "Behan Music",
+    imageUrl: "/img/sponsors/Behan%20Music.png",
+  },
+  {
+    size: "small",
+    name: "Drengr Axe Throwing",
+    imageUrl: "/img/sponsors/Drengr%20Axe%20Throwing.png",
+  },
+  {
+    size: "small",
+    name: "Fischer's Meat Market",
+    imageUrl: "/img/sponsors/Fischers%20Meat%20Market.png",
+  },
+  {
+    size: "small",
+    name: "Jump Around DFW",
+    imageUrl: "/img/sponsors/JumpAroundDFW.webp",
+  },
+  {
+    size: "small",
+    name: "Little Germany",
+    imageUrl: "/img/sponsors/Little%20Germany.png",
+  },
+  {
+    size: "small",
+    name: "Marquee Event Rentals",
+    imageUrl: "/img/sponsors/MarqueeEventRentals.svg",
+  },
+  {
+    size: "small",
+    name: "Moore Equipment Rental",
+    imageUrl: "/img/sponsors/Moore%20Equipment%20Rental.png",
+  },
+  {
+    size: "small",
+    name: "North Arlington Little League",
+    imageUrl: "/img/sponsors/NALL%20Logo.png",
+  },
+];
+
 export default function Home() {
   return (
     <main>
@@ -368,27 +446,53 @@ export default function Home() {
           <h3 className={`${lilita.className} text-center mb-16`}>
             Thank you to our sponsors
           </h3>
-          <div className="flex gap-8 justify-center">
-            <div>
-              <a href="https://www.barnesvideogroup.com/" target="_blank">
-                <Image
-                  src="/img/barnes-video-group-logo.png"
-                  alt="Rick and Ray's Auto Plaza"
-                  width={416}
-                  height={106}
-                />
-              </a>
-            </div>
-            <div>
-              <a href="https://rickandraysautoplaza.com/" target="_blank">
-                <Image
-                  src="/img/rick-and-rays-logo.png"
-                  alt="Rick and Ray's Auto Plaza"
-                  width={416}
-                  height={106}
-                />
-              </a>
-            </div>
+          <div className="flex flex-wrap gap-8 justify-center">
+            {sponsors
+              .filter((s) => s.size === "large")
+              .map((s) => (
+                <div
+                  key={s.imageUrl}
+                  className="relative flex-[100%] shrink-0 md:shrink h-[180px] md:flex-[30%]"
+                >
+                  <a href={s.linkUrl} target="_blank">
+                    <Image
+                      src={s.imageUrl}
+                      alt={s.name}
+                      fill={true}
+                      className="mx-auto object-contain"
+                    />
+                  </a>
+                </div>
+              ))}
+
+            {sponsors
+              .filter((s) => s.size === "small")
+              .map((s) => (
+                <div
+                  key={s.imageUrl}
+                  className="relative flex-[25%] shrink-0 md:shrink h-[90px] md:flex-[18%]"
+                >
+                  {s.linkUrl ? (
+                    <a href={s.linkUrl} target="_blank" className="bg-white">
+                      <Image
+                        src={s.imageUrl}
+                        alt={s.name}
+                        fill={true}
+                        className="mx-auto object-contain"
+                      />
+                    </a>
+                  ) : (
+                    <div className="bg-white">
+                      <Image
+                        src={s.imageUrl}
+                        alt={s.name}
+                        fill={true}
+                        className="mx-auto object-contain"
+                      />
+                    </div>
+                  )}
+                </div>
+              ))}
           </div>
         </div>
       </PageSection>
