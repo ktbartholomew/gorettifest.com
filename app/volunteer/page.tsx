bd2d1c77470aa0173e68f2e9685d0380ebeb0df8
Keith Bartholomew <keith.bartholomew@gmail.com>
2025-09-14 17:36:38 -0500

    volunteer page links to volunteerlocal

--- app/volunteer/page.tsx
+++ app/volunteer/page.tsx
@@ -5,7 +5,8 @@ import headerImage from "@/public/img/IMG_2256.webp";
 import { Button } from "@/components/button";
 import { lilita } from "@/components/font";
 
-const VOLUNTEER_SURVEY_LINK = "https://forms.gle/znyXijyCB2DFtAf88";
+const VOLUNTEER_SIGNUP_LINK =
+  "https://stmariagoretti.volunteerlocal.com/volunteer/?id=103729";
 const DONATION_LINK = "https://square.link/u/Ugurk7sW";
 
 export const metadata: Metadata = {
@@ -39,44 +40,48 @@ export default function VolunteerPage() {
                 GorettiFest needs your help!
               </h2>
               <p className="text-lg">
-                GorettiFest only exists because of your generous donations.
-                Every year, our entire parish comes together to volunteer their
-                time and resources to make GorettiFest an unforgettable event
-                for the entire community.
+                GorettiFest only exists because of your generous time and
+                donations. Every year, our entire parish comes together to
+                volunteer their time and resources to make GorettiFest an
+                unforgettable event for the entire community.
               </p>
               <div className="grid lg:grid-cols-2 gap-8 mt-16">
                 <div>
                   <h4>I want to help</h4>
                   <p>
-                    There are tons of different ways to volunteer at
-                    GorettiFest:
+                    GorettiFest can&apos;t happen without your help. Register to
+                    volunteer so we know that everyone who visits will have a
+                    great time. There are many ways you can help:
                   </p>
                   <ul className="list-disc pl-4">
                     <li>serving and preparing food</li>
-                    <li>supervising activity booths</li>
+                    <li>supervising game booths</li>
                     <li>setting up festival buildings and furniture</li>
                     <li>cleaning up during/after the festival</li>
                   </ul>
                   <p>
-                    Fill out this quick survey to help us match you with the
-                    volunteer opportunities that fit you best!
+                    We&apos;re using <strong>Volunteer Local</strong> to
+                    schedule volunteers so you know exactly when to show up and
+                    what to do. Sign up and register for the shifts you&apos;re
+                    available for on our Volunteer Local page.
                   </p>
                   <p className="text-center">
                     <a
-                      href={VOLUNTEER_SURVEY_LINK}
+                      href={VOLUNTEER_SIGNUP_LINK}
                       target="_blank"
                       rel="noreferrer"
                     >
-                      <Button>Take the Volunteer Survey</Button>
+                      <Button>Sign up at Volunteer Local</Button>
                     </a>
                   </p>
                 </div>
                 <div>
                   <h4>I want to donate money &amp; supplies</h4>
                   <p>
-                    A festival like GorettiFest doesn&apos;t happen for free;
-                    every donation you make helps our festival raise even more
-                    for Saint Maria Goretti Catholic School!
+                    Every donation you make helps our festival raise even more
+                    for Saint Maria Goretti Catholic School! Whether you have
+                    specific supplies to donate or would like to make a monetary
+                    donation, we appreciate your support!
                   </p>
                   <p className="text-center">
                     <a href={DONATION_LINK} target="_blank" rel="noreferrer">
