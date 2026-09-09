284f30f4dde6f1ba5b80762ae8b61320f19ff921
Keith Bartholomew <keith.bartholomew@gmail.com>
2026-09-08 19:09:02 -0500

    update supplies donation link to SignUpGenius

--- app/volunteer/page.tsx
+++ app/volunteer/page.tsx
@@ -88,7 +88,11 @@ export default function VolunteerPage() {
                     </a>
                   </p>
                   <p className="text-center">
-                    <a href="mailto:questions@gorettifest.com?subject=GorettiFest%20Supplies%20Donation">
+                    <a
+                      href="https://www.signupgenius.com/go/20F0448ADA622A5FE3-65669857-gorettifest"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
                       <Button>Donate supplies</Button>
                     </a>
                   </p>
